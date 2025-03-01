73c4234fd4484253ee6bc89d2cb61292152bf40e
Mahmoud Elabady <melabady199@gmail.com>
2025-03-02 00:40:00 +0200

    feat: add new logo images and update Header component layout

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 flex items-center justify-between pt-5">
-      <Input placeholder="Search" />
-      <nav>
-        <ul className="flex">
-          <li className="mr-4">
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
-      </nav>
-      <Button>Sign In</Button>
+    <header className="mx-auto p-4 bg-gray-200 flex justify-between items-center">
+      <Link href="/">
+        <span className="text-3xl font-mono tracking-widest first-letter:text-blue-900 first-letter:font-bold first-letter:text-4xl text-primary italic shadow-sm">
+          BookNest
+        </span>
+      </Link>
+      <button className="bg-transparent">
+        <Menu size={30} />
+      </button>
     </header>
   );
 };
